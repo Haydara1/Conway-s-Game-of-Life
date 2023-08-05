6c12214df97dbb5227c90147aaef552196073355
Haydara <haydaraal2005@gmail.com>
2023-08-05 10:50:19 +0200

    Added main function and variables

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Conway_s_Game_of_Life
+﻿using Raylib_cs;
+
+namespace Conway_s_Game_of_Life
 {
     internal class Program
     {
+        const int FPS = 60;
+
+        const int WIDTH = 64;
+        const int HEIGHT = 64;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Raylib.InitWindow(WIDTH, HEIGHT, "Conway's Game of Life");
+            Raylib.SetTargetFPS(FPS);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
         }
     }
 }
