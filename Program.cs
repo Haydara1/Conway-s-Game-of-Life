afdb27945aa6688fc6dc2a16c4eb3dcff4156219
Haydara <haydaraal2005@gmail.com>
2023-08-05 12:18:37 +0200

    Cleaned code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ namespace Conway_s_Game_of_Life
     internal class Program
     {
         const int InFPS = 64;
-        const int OutFPS = 4;
 
         const int WIDTH = 64;
         const int HEIGHT = 64;
@@ -15,6 +14,7 @@ namespace Conway_s_Game_of_Life
 
         static void Main(string[] args)
         {
+            int OutFPS = 4;
             bool Start = false;
 
             // Initializing the window.
@@ -32,34 +32,34 @@ namespace Conway_s_Game_of_Life
 
                 if (Start)
                     screen = NextCycle(screen);
-                
 
-                else
+                #region Input
+
+                Vector2 pos = Raylib.GetMousePosition();
+                int i = (int)Math.Floor(pos.X / SCALE);
+                int j = (int)Math.Floor(pos.Y / SCALE);
+
+                if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_RIGHT_BUTTON))
+                    screen[i, j] = true;
+
+                else if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_BUTTON_LEFT))
+                    screen[i, j] = false;
+
+                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
                 {
-                    Vector2 pos = Raylib.GetMousePosition();
-                    int i = (int)Math.Floor(pos.X / SCALE);
-                    int j = (int)Math.Floor(pos.Y / SCALE);
-
-                    if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_RIGHT_BUTTON))
-                        screen[i, j] = true;
-
-                    else if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_BUTTON_LEFT))
-                        screen[i, j] = false;
-
-                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
-                    {
-                        Start = true;
-                        Raylib.SetTargetFPS(OutFPS);
-                    }
-                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_X))
-                    {
-                        Start = false;
-                        Raylib.SetTargetFPS(InFPS);
-                    }
-                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT) && Start == false)
-                        screen = NextCycle(screen);
-
+                    Start = true;
+                    Raylib.SetTargetFPS(OutFPS);
                 }
+                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_X))
+                {
+                    Start = false;
+                    Raylib.SetTargetFPS(InFPS);
+                        
+                }
+                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT) && Start == false)
+                    screen = NextCycle(screen);
+
+                #endregion
 
                 Draw(screen);
                 Raylib.EndDrawing();
@@ -70,7 +70,8 @@ namespace Conway_s_Game_of_Life
 
         static bool[,] NextCycle(bool[,] screen) 
         {
-            bool[,] returnable = screen.Clone() as bool[,];
+
+            bool[,] returnable = (bool[,])screen.Clone();
 
             for(int i = 1; i < WIDTH - 1; i++)
             {
