ebe200f3ec5f0e3906ed57838ead85764ab6125f
Haydara <haydaraal2005@gmail.com>
2023-08-05 11:48:02 +0200

    Added Drawing function

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using Raylib_cs;
+using System.Numerics;
 
 namespace Conway_s_Game_of_Life
 {
     internal class Program
     {
-        const int FPS = 60;
+        const int FPS = 1;
 
         const int WIDTH = 64;
         const int HEIGHT = 64;
@@ -13,6 +14,8 @@ namespace Conway_s_Game_of_Life
 
         static void Main(string[] args)
         {
+            bool Start = false
+
             // Initializing the window.
             Raylib.InitWindow(WIDTH * SCALE, HEIGHT * SCALE, "Conway's Game of Life");
             Raylib.SetTargetFPS(FPS);
@@ -20,11 +23,30 @@ namespace Conway_s_Game_of_Life
             // Representing the screen within an array.
             bool[,] screen = new bool[WIDTH, HEIGHT];
 
+            //Test:
+            screen[20, 20] = true;
+            screen[20, 22] = true;
+            screen[20, 21] = true;
+
             // Main loop.
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
+
+                if (Start)
+                {
+                    Draw(screen);
+                    screen = NextCycle(screen);
+                }
+                else
+                {
+                    if(Raylib.IsMouseButtonPressed(0)) 
+                    {
+                        Vector2 pos = Raylib.GetMousePosition();
+                    }
+                }
+                
 
                 Raylib.EndDrawing();
             }
@@ -34,14 +56,14 @@ namespace Conway_s_Game_of_Life
 
         static bool[,] NextCycle(bool[,] screen) 
         {
-            bool[,] returnable = screen;
+            bool[,] returnable = screen.Clone() as bool[,];
 
             for(int i = 1; i < WIDTH - 1; i++)
             {
                 for(int j = 1; j < HEIGHT - 1; j++)
                 {
                     // Check the number of the living neighbours.
-                    short counter = 0;
+                    int counter = 0;
 
                     if (screen[i + 1, j])
                         counter++;
@@ -62,7 +84,7 @@ namespace Conway_s_Game_of_Life
 
                     if (screen[i, j]) // Alive cell.
                     {
-                        if (counter > 3 || counter == 1)
+                        if (counter > 3 || counter < 2)
                             returnable[i, j] = false;
                     }
                     else // Dead cell.
@@ -75,6 +97,18 @@ namespace Conway_s_Game_of_Life
 
 
             return returnable;
+        }
+
+        static void Draw(bool[,] screen)
+        {
+            for (int i = 1; i < WIDTH - 1; i++)
+            {
+                for (int j = 1; j < HEIGHT - 1; j++)
+                {
+                    if (screen[i, j])
+                        Raylib.DrawRectangle(i * SCALE, j * SCALE, SCALE, SCALE, Color.BLACK);
+                }
+            }
         }
     }
 }
